1b18a7a6dcd31c2d6e8a45c25f87ac26828e4d0b
Pedro Viana <pedroviana.developer13@gmail.com>
2023-07-20 15:51:39 -0300

    feat: get main menu def

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -24,6 +24,16 @@ class MenuBuilder:
 
         self.inventory.consume_recipe(curr_dish.recipe)
 
-    # Req 4
+# Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        return [
+            {
+                "dish_name": dish.name,
+                "price": dish.price,
+                "ingredients": dish.get_ingredients(),
+                "restrictions": dish.get_restrictions(),
+            }
+            for dish in self.menu_data.dishes
+            if restriction is None or
+            restriction not in dish.get_restrictions()
+        ]
